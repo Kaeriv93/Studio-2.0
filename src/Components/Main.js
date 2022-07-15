c09284dc138e56ee2940c050b87105ca079a42c5
kaeriv93 <vsan93@yahoo.com>
2022-07-15 12:25:41 -0500

    initialized main component into react app

---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+// import { useEffect, useState} from "react";
+// import {Routes, Route} from 'react-router-dom'
+
+function Main(){
+    return(
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+
+export default Main
