6b12d16bb042fc5ee422fa9f2aec7c209f163ab4
kaeriv93 <vsan93@yahoo.com>
2022-07-16 11:22:03 -0500

    Header is working well

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Components/Header';
 import Main from './Components/Main';
 
 function App() {
   return (
     <>
+    <Header/>
     <Main/>
     </>
   );
