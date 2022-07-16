bf3dff969e64b10cb2d5490e59366b16e0b1ba08
kaeriv93 <vsan93@yahoo.com>
2022-07-16 10:35:59 -0500

    api call is working

---
+++ src/Components/Film.js
@@ -0,0 +1,9 @@
+function Film(props){
+    console.log(props)
+ return(
+    <div>
+        <h1>Hello World!</h1>
+    </div>
+ )   
+}
+export default Film
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,25 @@
-// import { useEffect, useState} from "react";
-// import {Routes, Route} from 'react-router-dom'
+import { useEffect, useState} from "react";
+import {Routes, Route} from 'react-router-dom'
+import Film from "./Film";
 
 function Main(){
+    const[film, setFilm] = useState(null)
+    const URL = "https://ghibliapi.herokuapp.com/films";
+
+    const getFilm = () =>{
+        fetch(URL)
+        .then(response => response.json())
+        .then(result => setFilm(result))
+        console.log(setFilm)
+    }
+    useEffect(()=> getFilm(),[])
+
+
     return(
         <>
-            <h1>Hello World</h1>
+        <Routes>
+            <Route path ='/' element = {<Film film ={film}/>}/>
+        </Routes>
         </>
     )
 }
