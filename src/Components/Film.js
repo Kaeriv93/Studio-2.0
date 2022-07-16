16c00d8e142288e7be8dea0148acf6e96f77e40d
kaeriv93 <vsan93@yahoo.com>
2022-07-16 10:39:36 -0500

    Api call is dynamically displaying all images and data

--- src/Components/Film.js
+++ src/Components/Film.js
@@ -1,9 +1,16 @@
 function Film(props){
-    console.log(props)
- return(
-    <div>
-        <h1>Hello World!</h1>
-    </div>
- )   
+    const loaded = () =>{
+        return props.film.map((film,idx)=>(
+            <main>
+                <section>
+                    <div key={idx}>
+                        <h1>{film.title}</h1>
+                        <img src={film.image} alt={film.name}/>
+                    </div>
+                </section>
+            </main>
+        ))
+    }
+ return props.film ? loaded():<h1>Loading...</h1>
 }
 export default Film
